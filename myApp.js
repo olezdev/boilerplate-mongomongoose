7dbfa5a4bdc7e9fa0fa9a25377ae3e096083c016
Leonardo Olivera <olezdev@gmail.com>
2023-12-11 16:02:11 -0300

    Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -11,7 +11,14 @@ const personSchema = new mongoose.Schema({
 let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var person = new Person({
+    name: "olezdev",
+    age: 35,
+    favoriteFoods: ["pizza", "tofu", "garbanzos"]
+  })
+  person.save(function (err, data) {
+    done(null, data);
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
