3eb313327a75962a592ebc529e7e79367ad81b65
Leonardo Olivera <olezdev@gmail.com>
2023-12-12 14:47:48 -0300

    edit chain search query

--- myApp.js
+++ myApp.js
@@ -93,13 +93,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  let findName = Person.find({ name: "Leah" });
-  findName
+  Person.find({ favoriteFoods: foodToSearch })
     .sort({ name: 1 })
     .limit(2)
     .select({ name: 1, age: 0, favoriteFoods: 1 })
-    .exec(function (err, person) {
-      if (err) return console.log(err);
+    .exec((err, person) => {
+      if (err) {
+        return console.log(err);
+      }
       done(null, person);
     })
 };
